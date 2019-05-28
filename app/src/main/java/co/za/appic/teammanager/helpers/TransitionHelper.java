24b44a806487d59bc7f771bf44759a58a4d935fd
TLB <rocboyt@gmail.com>
2019-05-28 20:01:32 +0200

    Added transition helper

--- app/src/main/java/co/za/appic/teammanager/helpers/TransitionHelper.java
+++ app/src/main/java/co/za/appic/teammanager/helpers/TransitionHelper.java
@@ -1,8 +1,17 @@
 package co.za.appic.teammanager.helpers;
 
+import android.content.Context;
+
+import co.za.appic.teammanager.R;
+
 public class TransitionHelper {
     private int inAnimation;
     private int outAnimation;
+    private Context context;
+
+    public  TransitionHelper(Context context) {
+        this.context = context;
+    }
 
     public int getInAnimation() {
         return inAnimation;
@@ -15,5 +24,27 @@ public class TransitionHelper {
     }
     public void setOutAnimation(int outAnimation) {
         this.outAnimation = outAnimation;
+    }
+
+    protected TransitionHelper slideInActivity() {
+        return getTransitionAnimation(R.anim.slide_right, R.anim.no_transition);
+    }
+
+    protected TransitionHelper slideOutActivity() {
+        return getTransitionAnimation(R.anim.no_transition, R.anim.slide_left);
+    }
+
+    protected TransitionHelper fadeInActivity() {
+        return getTransitionAnimation(R.anim.fade_in, R.anim.no_transition);
+    }
+
+    public TransitionHelper fadeOutActivity() {
+        return getTransitionAnimation(R.anim.no_transition, R.anim.fade_out);
+    }
+
+    private TransitionHelper getTransitionAnimation(int inAnimation, int outAnimation) {
+        this.setInAnimation(inAnimation);
+        this.setOutAnimation(outAnimation);
+        return this;
     }
 }
