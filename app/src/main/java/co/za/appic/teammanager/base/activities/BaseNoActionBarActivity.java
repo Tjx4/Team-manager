04505fea429a53a366155beb34c0d66183418727
TLB <rocboyt@gmail.com>
2019-05-28 21:01:00 +0200

    Added base no actionbar activity

---
+++ app/src/main/java/co/za/appic/teammanager/base/activities/BaseNoActionBarActivity.java
@@ -0,0 +1,10 @@
+package co.za.appic.teammanager.base.activities;
+
+import android.os.Bundle;
+
+public abstract class BaseNoActionBarActivity extends BaseAsyncActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
