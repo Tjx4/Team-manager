09165f4bf83f89704026e5696cb9d75fa7da06e0
TLB <rocboyt@gmail.com>
2019-05-28 21:01:41 +0200

    Modified signin activity

--- app/src/main/java/co/za/appic/teammanager/features/signin/SignInActivity.java
+++ app/src/main/java/co/za/appic/teammanager/features/signin/SignInActivity.java
@@ -1,15 +1,31 @@
 package co.za.appic.teammanager.features.signin;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.View;
 import co.za.appic.teammanager.R;
+import co.za.appic.teammanager.base.activities.BaseNoActionBarActivity;
 
-public class SignInActivity extends AppCompatActivity {
+public class SignInActivity extends BaseNoActionBarActivity implements SignInView{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
     }
+
+    @Override
+    protected void setBaseActivityDependencies() {
+        setContentView(R.layout.activity_sign_in);
+        initViews();
+    }
+    
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    public void onLoginButtonClicked(View view) {
+
+    }
+
 }
---
+++ app/src/main/java/co/za/appic/teammanager/features/signin/SignInView.java
@@ -0,0 +1,7 @@
+package co.za.appic.teammanager.features.signin;
+
+import android.view.View;
+
+public interface SignInView {
+    void onLoginButtonClicked(View view);
+}
