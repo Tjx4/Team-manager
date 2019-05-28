aa726545505749d876d4c9e6d54d0632b9f9320d
TLB <rocboyt@gmail.com>
2019-05-28 19:04:44 +0200

    Added custom toast

---
+++ app/src/main/java/co/za/appic/teammanager/base/activities/BaseActivity.java
@@ -0,0 +1,75 @@
+package co.za.appic.teammanager.base.activities;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import co.za.appic.teammanager.R;
+import co.za.appic.teammanager.base.presenters.BasePresenter;
+import co.za.appic.teammanager.constants.Constants;
+import co.za.appic.teammanager.helpers.DialogFragmentHelper;
+import co.za.appic.teammanager.helpers.PermissionsHelper;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    protected Context context;
+    protected BasePresenter presenter;
+    protected PermissionsHelper permissionsHelper;
+    protected DialogFragmentHelper dialogFragmentHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = this;
+        permissionsHelper = new PermissionsHelper(this);
+        setBaseActivityDependencies();
+
+        try {
+            int[] activityTransition = getIntent().getBundleExtra(Constants.PAYLOAD_KEY).getIntArray(Constants.ACTIVITY_TRANSITION);
+            overridePendingTransition(activityTransition[0], activityTransition[1]);
+        } catch (Exception e) {
+
+        }
+    }
+
+    protected abstract void setBaseActivityDependencies();
+
+    public void showShortToast(String message) {
+        Toast toast = getToast(message, Toast.LENGTH_SHORT);
+        showTheToastIfNotNull(toast);
+    }
+
+    public void showLongToast(String message) {
+        Toast toast = getToast(message, Toast.LENGTH_LONG);
+        showTheToastIfNotNull(toast);
+    }
+
+    private void showTheToastIfNotNull(Toast toast){
+        if (toast != null)
+            toast.show();
+    }
+
+    private Toast getToast(String message, int length) {
+        try {
+            LayoutInflater inflater = ((Activity) context).getLayoutInflater();
+            View layout = inflater.inflate(R.layout.custom_toast, (ViewGroup) ((Activity) context).findViewById(R.id.root));
+            TextView text = layout.findViewById(R.id.toast_message);
+            text.setText(message);
+            Toast toast = new Toast(context);
+            toast.setView(layout);
+            toast.setDuration(length);
+
+            return toast;
+        } catch (Exception e) {
+            Log.e(Constants.TOAST_ERROR, "" + e);
+            return null;
+        }
+    }
+
+}
